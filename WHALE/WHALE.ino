4bd9e3c2c8557b208a9cab15b12a98ecb19bd41c
mizuyoukan-ao <48982871+mizuyoukanao@users.noreply.github.com>
2022-05-27 23:01:24 +0900

    サーボの挙動調査のためattach文を移動

--- WHALE/WHALE.ino
+++ WHALE/WHALE.ino
@@ -10,7 +10,7 @@ Gamecube_Data_t d = defaultGamecubeData;
 void setup()
 {
   //SG90用初期セットアップ
-  myservo.attach(SV_PIN, 500, 2400);
+  //myservo.attach(SV_PIN, 500, 2400);
 
   d.report.a = 0;
   d.report.b = 0;
@@ -194,10 +194,13 @@ void update_AT12806379(char m){
 void press_gc_reset(){
   //角度は各々要調整
   Serial.println("Reset");
+  myservo.attach(SV_PIN, 500, 2400);
+  delay(500);
   myservo.write(65);
   delay(500);
   myservo.write(90);
   delay(500);
+  myservo.detach();
 }
 
 void update_data(){
